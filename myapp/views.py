ce48a0b0d2871285e2e13b00e11371333c2470eb
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-03-30 01:11:59 -0700

    add search

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,23 @@
+from datetime import datetime
+
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+
 from myapp.models import Person
 
 
+@csrf_protect
 def home(request):
     persons = Person.objects.all()
+    if 'search' in request.GET:
+        key = request.GET.get('search')
+        persons = Person.objects.all().filter(Q(name__icontains=key), Q(description__icontains=key))
+    else:
+        persons = Person.objects.all()
+
     return render(request, 'myapp/index.html', {'persons': persons})
+
+
+
